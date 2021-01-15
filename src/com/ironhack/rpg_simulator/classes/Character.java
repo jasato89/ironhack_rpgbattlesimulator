61f078b123b72c1742588552a83cc05f6b838c2c
anjenaex <anjenaex@gmail.com>
2021-01-15 20:03:59 +0100

    Character, Warrior y Wizard con la info básica y funcionando

--- src/com/ironhack/rpg_simulator/classes/Character.java
+++ src/com/ironhack/rpg_simulator/classes/Character.java
@@ -1,50 +1,57 @@
 package com.ironhack.rpg_simulator.classes;
 
-public abstract class Character{
+public abstract class Character {
 
-//propiedades
+        //propiedades
 //El id es static, porque tiene que ser un idenficador único, independientemente del número de instancias
-private static int id = 0;
-private String name;
-private int hp;
-private boolean isAlive = true;
+        private static int id = 0;
+        private String name;
+        private int hp;
+        private boolean isAlive = true;
 
-public Character(String name, int hp, boolean isAlive) {
-        id++;
-        setName(name);
-        setHp(hp);
-        setAlive(isAlive);
-        }
-
-        // Creamos un método no estático, para recibir el daño
-       // public void damage(int damage) {};
-//getters and setters
-public static int getId() {
-        return id;
+        public Character(String name, int hp, boolean isAlive) {
+                id++;
+                setName(name);
+                setHp(hp);
+                setAlive(isAlive);
         }
 
-public String getName() {
-        return name;
+        // Creamos un método no estático, para recibir el daño. No es abstracto porque es igual en las clases que heredan
+        public void damage(int damage) {
+                this.hp -= damage;
+                if(this.hp <= 0) {
+                        this.isAlive = false;
+                }
+        };
+
+        //getters and setters
+
+        public static int getId() {
+                return id;
         }
 
-public void setName(String name) {
-        this.name = name;
+        public String getName() {
+                return name;
         }
 
-public int getHp() {
-        return hp;
+        public void setName(String name) {
+                this.name = name;
         }
 
-public void setHp(int hp) {
-        this.hp = hp;
+        public int getHp() {
+                return hp;
         }
 
-public boolean isAlive() {
-        return isAlive;
+        public void setHp(int hp) {
+                this.hp = hp;
         }
 
-public void setAlive(boolean alive) {
-        isAlive = alive;
+        public boolean isAlive() {
+                return isAlive;
         }
+
+        public void setAlive(boolean alive) {
+                isAlive = alive;
         }
+}
 
