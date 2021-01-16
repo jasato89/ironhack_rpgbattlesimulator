de4e7f7c4ae2f5cf98e1d607a50c1129bfce80b2
Iván Trujillo <ivtrujillo12@gmail.com>
2021-01-16 22:10:59 +0000

    Added CSV files with parties and updated importParty

--- src/com/ironhack/rpg_simulator/classes/importParty.java
+++ src/com/ironhack/rpg_simulator/classes/importParty.java
@@ -3,6 +3,7 @@ package com.ironhack.rpg_simulator.classes;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class importParty {
     public importParty() {}
@@ -10,7 +11,7 @@ public class importParty {
     public static Party importPartyCsv(File csvFile) {
         try {
             Scanner scannerCsv = new Scanner(csvFile);
-            Party partyCsv = new Party();
+            ArrayList<Character> partyArray = new ArrayList<Character>();
 
             scannerCsv.nextLine();
             while(scannerCsv.hasNextLine()) {
@@ -19,14 +20,15 @@ public class importParty {
                 if(dataCharacter[6].equals("")) {
                     Warrior warrior = new Warrior(dataCharacter[2], Integer.parseInt(dataCharacter[3]), true,
                             Integer.parseInt(dataCharacter[4]), Integer.parseInt(dataCharacter[5]));
-                    partyCsv.addCharacter(warrior);
+                    partyArray.add(warrior);
                 } else {
                     Wizard wizard = new Wizard(dataCharacter[2], Integer.parseInt(dataCharacter[3]), true,
                             Integer.parseInt(dataCharacter[6]), Integer.parseInt(dataCharacter[7]));
-                    partyCsv.addCharacter(wizard);
+                    partyArray.add(wizard);
                 }
             }
             scannerCsv.close();
+            Party partyCsv = new Party(partyArray);
             return partyCsv;
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
