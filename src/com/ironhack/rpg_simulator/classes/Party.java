39f471120e076bbb3da7f9fe918e2cdf2892a2c1
Jaume <jasato89@gmail.com>
2021-01-16 16:20:57 +0100

    Added feature that enables to add a Jr. in case the name is repeated

--- src/com/ironhack/rpg_simulator/classes/Party.java
+++ src/com/ironhack/rpg_simulator/classes/Party.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class Party {
 
-    ArrayList<Character> party = new ArrayList<Character>();
     private final int PARTY_LIMIT = 10;
+    ArrayList<Character> party;
 
 
     public Party(ArrayList<Character> party) {
@@ -18,7 +18,7 @@ public class Party {
     }
 
     public void getAllMembers() {
-        int i = 0;
+        int i = 1;
         for (Character character : party) {
             System.out.println(i + " - " + character.toString());
             i++;
@@ -26,7 +26,7 @@ public class Party {
     }
 
     public Object getMemberById(int id) {
-        return party.get(id);
+        return party.get(id - 1);
     }
 
     public ArrayList<Character> getParty() {
@@ -35,21 +35,34 @@ public class Party {
     }
 
     public ArrayList<Character> generateRandomParty() {
-        party.clear();
+        party = new ArrayList<Character>();
         for (int i = 0; i < PARTY_LIMIT; i++) {
             boolean bool = new Random().nextBoolean();
             if (bool) {
                 Character warrior = RandomCharacterDatabase.getRandomWarrior();
+                if (!party.isEmpty()) {
+                    for (Character name : party) {
+                        if (warrior.getName().equals(name.getName())) {
+                            warrior.setName(warrior.getName() + " Jr.");
+                        }
+                    }
+                }
                 party.add(warrior);
             } else {
                 Character wizard = RandomCharacterDatabase.getRandomWizard();
+                if (!party.isEmpty()) {
+                    for (Character name : party) {
+                        if (wizard.getName().equals(name.getName())) {
+                            wizard.setName(wizard.getName() + " Jr.");
+                        }
+                    }
+                }
                 party.add(wizard);
             }
         }
+
         return party;
     }
-
-
 
 
 }
